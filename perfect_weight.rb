93458985723f6e6d707fe6849df119782af733db
LizaMatiash <80411353+LizaMatiash@users.noreply.github.com>
2022-07-12 16:18:27 +0300

    Delete perfect_weight.rb

--- perfect_weight.rb
+++
@@ -1,5 +0,0 @@
-puts "Your name: "
-name = gets.chomp
-puts "Your height: "
-height = gets.chomp
-puts (height.to_i - 110) > 0 ? "#{name}, your perfect weight is #{height.to_i - 110}" : "Your weight is optimal"
