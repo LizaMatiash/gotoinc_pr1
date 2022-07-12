1a5280b0c61fe057097ee5716e1320ffc5ec605a
LizaMatiash <80411353+LizaMatiash@users.noreply.github.com>
2022-07-12 11:20:10 +0300

    Add files via upload

---
+++ equation.rb
@@ -0,0 +1,15 @@
+puts "For ах^2 + bx + c = 0 enter a: "
+a = gets.chomp.to_i
+puts "Enter b: "
+b = gets.chomp.to_i
+puts "Enter c: "
+c = gets.chomp.to_i
+
+dis = b**2 - 4*a*c
+if dis < 0
+  puts "D < 0, no x1, x2"
+elsif dis = 0
+  puts "D = 0, x1 = x2 = #{ -b / (2 * a)}"
+else
+  puts "D > 0, x1 = #{(-b + c) / (2 * a) }, x2 = #{(-b - c) / (2 * a)}"
+end
